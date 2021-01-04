a3df47cc4243656dd2ab8ce8e9bd61d4cc313dd0
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-01-04 17:37:10 +0500

    init

---
+++ src/components/App/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { FunctionComponent } from "react";
+
+const App: FunctionComponent = () => {
+    return <div>Hello World</div>
+}
+
+export default App;
---
+++ webpack.common.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry:  path.join(__dirname, 'src', 'index.tsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    target: 'web',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template : path.join(__dirname, 'src', 'index.html')})
+    ]
+}
