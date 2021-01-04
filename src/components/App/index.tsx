453c199dc75dec1734f7bbce8ab5cfd97522dd08
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-01-04 22:02:12 +0500

    test hooks

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FunctionComponent } from "react";
 
 const App: FunctionComponent = () => {
-    return <div>Hello World</div>
+    return <div>Hello World!</div>
 }
 
 export default App;
