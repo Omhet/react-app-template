aa246c2206c5d4e1c1bf57de6954e1cbc215a000
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-01-04 21:55:01 +0500

    lint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        node: true,
+        commonjs: true,
+        browser: true,
+        es6: true
+    },
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint'
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    rules: {
+        '@typescript-eslint/no-var-requires': 0
+    }
+}
