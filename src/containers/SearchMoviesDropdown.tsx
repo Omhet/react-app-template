f8ae7224883639ebef65b053275c01a8e830ec60
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-01-07 14:39:14 +0500

    search movies

--- src/containers/SearchMoviesDropdown.tsx
+++ src/containers/SearchMoviesDropdown.tsx
@@ -1,14 +1,60 @@
 import { Select } from 'antd';
-import React, { FunctionComponent } from 'react';
+import { LabeledValue } from 'antd/lib/select';
+import React, {
+    FunctionComponent,
+    useCallback,
+    useEffect,
+    useState,
+} from 'react';
+
+export const fetchMovies = async ({
+    query,
+}: {
+    query: string;
+}): Promise<string[]> => {
+    const res = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
+    );
+    const data = await res.json();
+    return data.results.map(({ title }: { title: string }) => title);
+};
 
 const Container: FunctionComponent = () => {
+    const [query, setQuery] = useState('');
+    const [options, setOptions] = useState<LabeledValue[]>([]);
+    const [loading, setLoading] = useState(false);
+
+    const handleInputChange = useCallback((value: string) => {
+        setQuery(value);
+    }, []);
+
+    useEffect(() => {
+        const searchMovies = async () => {
+            if (query.length === 0) {
+                return;
+            }
+            setLoading(true);
+            const results = await fetchMovies({ query });
+            const options = results.map((title, index) => ({
+                value: title,
+                label: title,
+                key: String(index),
+            }));
+            setOptions(options);
+            setLoading(false);
+        };
+        searchMovies();
+    }, [query]);
+
     return (
         <Select
             showSearch
             placeholder="Search a movie"
             style={{ width: 200 }}
-            open
-            options={[{ label: 'Inter', value: 'inter' }]}
+            loading={loading}
+            options={options}
+            value={query}
+            onSearch={handleInputChange}
         />
     );
 };
