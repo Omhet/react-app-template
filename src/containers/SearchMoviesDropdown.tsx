a26a63eb7ae7e4b46d921122bea6b9d6f338e8ae
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-01-08 14:12:31 +0500

    axios

--- src/containers/SearchMoviesDropdown.tsx
+++ src/containers/SearchMoviesDropdown.tsx
@@ -1,5 +1,6 @@
 import { Input, Select } from 'antd';
 import { LabeledValue } from 'antd/lib/select';
+import axios from 'axios';
 import { debounce } from 'lodash-es';
 import React, { FunctionComponent, useCallback, useState } from 'react';
 
@@ -8,10 +9,18 @@ export const fetchMovies = async ({
 }: {
     query: string;
 }): Promise<string[]> => {
-    const res = await fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
+    const { data } = await axios.get(
+        'https://api.themoviedb.org/3/search/movie',
+        {
+            params: {
+                api_key: process.env.TMDB_API_KEY,
+                language: 'en-US',
+                query,
+                page: '1',
+                include_adult: 'false',
+            },
+        }
     );
-    const data = await res.json();
     return data.results.map(({ title }: { title: string }) => title);
 };
 
@@ -21,7 +30,7 @@ const useDebounce = (callback: (...args: any) => any, delay: number) =>
 const Container: FunctionComponent = () => {
     const [options, setOptions] = useState<LabeledValue[]>([]);
     const [loading, setLoading] = useState(false);
-    const [delay, setDelay] = useState(0);
+    const [delay, setDelay] = useState(1000);
 
     const handleInputChange = (value: string) => {
         debouncedSearch(value);
